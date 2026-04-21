9ef5b839e1c6a53cc32fd64a42c7556cf92b2cd1
MatteoDelMonaco <s311223@studenti.polito.it>
2026-04-21 18:11:37 +0200

    feat: added automatic compilation for plantuml architecture diagrams

---
+++ tools/puml2img/compile-architecture.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Must be executed from project's root directory. 
+java -jar ~/Downloads/plantuml-gplv2-1.2026.2.jar src/diagrams/architecture/ -o ../../../img/diagrams/architecture/
